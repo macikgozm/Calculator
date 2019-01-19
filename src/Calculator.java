42bc080dc6154a903a435de9a08146426704012d
Mehmet Acikgoz <macikgozm@gmail.com>
2019-01-19 12:06:39 -0600

    add ..

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,12 +4,14 @@
 public class Calculator {
 
     public static double add(double num1, double num2){
+
         return num1+num2;
     }
 
 
     public static double subtract(double num1, double num2){
         return num1-num2;
+
     }
     
     // add multiply method
